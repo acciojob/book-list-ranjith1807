a1e0b797fadc94f2fb7c4e024e5033427f304f4f
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-04-02 21:41:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+
+function addrow() {
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  const bookList = document.getElementById('book-list');
+
+  const row = document.createElement('tr');
+
+
+  row.innerHTML = `
+    <td>${title}</td>
+    <td>${author}</td>
+    <td>${isbn}</td>
+    <td><button class="delete">X</button></td>
+  `;
+
+
+  bookList.appendChild(row);
+}
+
+
+document.getElementById('book-list').addEventListener('click', function(e) {
+
+  if (e.target.classList.contains('delete')) {
+
+    e.target.parentElement.parentElement.remove();
+  }
+});
